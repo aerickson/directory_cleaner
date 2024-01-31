5cf950973ae71df7213dfd4ed2998262909e8dc2
Andrew Erickson <aerickson@mozilla.com>
2024-01-31 18:09:32 -0500

    refactor name

--- directory_cleaner_test.py
+++ directory_cleaner_test.py
@@ -16,28 +16,27 @@ class TestFileOperations(unittest.TestCase):
 
     def setUp(self):
         # test dir 1
-        self.temp_dir = tempfile.mkdtemp()
+        self.temp_dir1 = tempfile.mkdtemp()
 
         files_and_directories1 = ["junk1", "junk2", "junk_dir/junk4",
                                   "caches/cache1/blah2", "caches/cache2/blah"
                                   "tasks/task1/task123", "tasks/task2/task234",]
-        self.create_folders_and_files(self.temp_dir, files_and_directories1)
+        self.create_folders_and_files(self.temp_dir1, files_and_directories1)
 
     def tearDown(self):
         # Remove the temporary directory and its contents after the test
-        shutil.rmtree(self.temp_dir)
+        shutil.rmtree(self.temp_dir1)
 
     def test_directory_cleaner(self):
-        # directory_path = "/tmp/test-directory"
         exception_list = ["generic-worker.cfg", "tasks", "caches"]
-        dc = directory_cleaner.DirectoryCleaner(self.temp_dir, exception_list)
+        dc = directory_cleaner.DirectoryCleaner(self.temp_dir1, exception_list)
         result = dc.clean_directory()
-        print(result)
-        assert result['deleted'] == [ str(Path(self.temp_dir) / 'junk2'), 
-                                     str(Path(self.temp_dir) / 'junk1'), 
-                                     str(Path(self.temp_dir) / 'junk_dir/junk4')]
-        assert result['skipped'] == [ str(Path(self.temp_dir) / 'tasks'), 
-                                     str(Path(self.temp_dir) / 'caches')]
+        # print(result)
+        assert result['deleted'] == [ str(Path(self.temp_dir1) / 'junk2'), 
+                                     str(Path(self.temp_dir1) / 'junk1'), 
+                                     str(Path(self.temp_dir1) / 'junk_dir/junk4')]
+        assert result['skipped'] == [ str(Path(self.temp_dir1) / 'tasks'), 
+                                     str(Path(self.temp_dir1) / 'caches')]
 
 if __name__ == '__main__':
     unittest.main()
