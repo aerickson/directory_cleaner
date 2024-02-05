d8b921290365c983215059ecb13dcdf425675d1b
Andrew Erickson <aerickson@mozilla.com>
2024-02-05 13:39:14 -0500

    more work on args and configs

--- directory_cleaner/main.py
+++ directory_cleaner/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import toml
+import textwrap
 
 import directory_cleaner.directory_cleaner as DC
 
@@ -12,12 +13,23 @@ def parse_config_file(config_path):
 
 def main():
     # Create ArgumentParser object
-    parser = argparse.ArgumentParser(description='Delete items in a directory while excluding certain items.')
+    parser = argparse.ArgumentParser(description='Delete items in a directory while excluding certain items.',
+                                           formatter_class=argparse.RawDescriptionHelpFormatter,
+      epilog=textwrap.dedent('''\
+         config file format (toml):
+
+            exclusion_patterns = [
+                            'directory_a',  # all files in this directory be excluded from deletion
+                            'file_xyz',
+                        ]                             
+
+         '''))
 
     # Add command line argument for config file path
     parser.add_argument('--config', '-c', dest='config_file', type=str, required=True,
                         help='Path to the config file in INI format.')
     parser.add_argument('--dry-run', '-d', dest='dry_run', action='store_true', help="Don't delete anything, just print what would be deleted.")
+    parser.add_argument('directory', type=str, help='The directory to clean.')
 
     # Parse command line arguments
     args = parser.parse_args()
