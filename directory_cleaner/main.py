2614e220310e153c0c5989b39ea09927408f4976
Andrew Erickson <aerickson@mozilla.com>
2024-07-24 12:16:01 -0700

    tweak description

--- directory_cleaner/main.py
+++ directory_cleaner/main.py
@@ -41,7 +41,7 @@ def get_version_from_pyproject():
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Delete items in a directory while excluding certain items.",
+        description="Remove all files and directories not in an exception list (in a specific directory).",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent(
             """\
